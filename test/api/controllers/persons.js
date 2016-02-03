4c96815f4e4f3f410eabc231e68722a29a61e382
Brian Ly <brian.c.ly@accenture.com>
2016-02-03 10:04:06 -0800

    Updated one of the tests for the persons controller

--- test/api/controllers/persons.js
+++ test/api/controllers/persons.js
@@ -2,6 +2,8 @@ var should = require('should');
 var request = require('supertest');
 var server = require('../../../app');
 
+var personService = require('../../../api/services/persons.js')
+
 process.env.A127_ENV = 'test';
 
 describe('controllers', function() {
@@ -13,14 +15,17 @@ describe('controllers', function() {
       it('should return information based on given id', function(done) {
 
         request(server)
-          .get('/persons/12345/events')
+          .get('/persons/9996/events')
           .set('Accept', 'application/json')
           .expect('Content-Type', /json/)
           .expect(200)
           .end(function(err, res) {
-            should.not.exist(err);
 
-            res.body.personId.should.eql(12345);
+            personService.getPersonbyId(9996, function(err, result) {
+                should.not.exist(err);
+                should.exist(result);
+                result.id.should.eql(9996);
+            })
 
             done();
           });
