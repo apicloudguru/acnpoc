6edb954ad969760ae097c8097a7759fa4bba91ce
Brian Ly <brian.c.ly@accenture.com>
2016-02-03 11:30:04 -0800

    Added events controller

---
+++ api/controllers/events.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var util = require('util');
+var request = require('request');
+var Event = require('../models/Event.js');
+var Person = require('../models/Person.js');
+var eventService = require('../services/events.js');
+var personService = require('../services/persons.js');
+
+module.exports = {
+  events: events
+};
+
+function events(req, res) {
+  personService.getListofIds(req.swagger.params.id.value, function(err, listofIds) {
+    if (err) {
+      res.send(err);
+    };
+    eventService.getEventsByPersonIds(listofIds, function(err, result) {
+      if (err) {
+        res.send(err);
+      };
+      res.json(result);
+    })
+  });
+}
